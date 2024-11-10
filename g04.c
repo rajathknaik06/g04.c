59880b83a761423fe7a83b1f47f2287a370fe4a4
Rajath K Naik <rajathkrishnamurthy173@gmail.com>
2024-11-10 18:30:00 +0530

    Create g04.c

---
+++ g04.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char section;
+    int rollnumber;
+    
+    scanf("%c",&section);        //a 56
+    
+    scanf("%d",&rollnumber);
+    
+    printf("Roll number %d in section %c\n",rollnumber ,section);
+    
+    return 0;
+}
